a13f34f33ee3dd8f8537813e6881b23a06d9c345
Arjun-murmu <murmuarjun88@gmail.com>
2025-07-04 08:06:51 +0530

    plan

--- DSA Q/merge_sort_array.py
+++ DSA Q/merge_sort_array.py
@@ -30,9 +30,11 @@ class Solution(object):
             idx -= 1
             j -= 1
 
-nums1 = [1, 2, 3, 0, 0, 0]
+# nums1 = [1, 2, 3, 0, 0, 0]
+nums1 = [4, 3, 5, 0, 0, 0]
 m = 3
-nums2 = [2, 5, 6]
+nums2 = [1, 2, 3]
+# nums2 = [2, 5, 6]
 n = 3
 
 Solution().merge(nums1, m, nums2, n)

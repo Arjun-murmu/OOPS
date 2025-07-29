9645e0a33e32966be67ee6a8b22e273c70efabef
Arjun-murmu <murmuarjun88@gmail.com>
2025-07-29 23:23:28 +0530

    patten print

---
+++ DSA Complate/LEC 4/patten_1.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*111111
+    222222
+    333333
+    444444
+    555555*/
+
+    // int n = 5;
+    // for(int i = 1;i<=n;i++){  //outer loop for rows
+    //     for(int j = 0;j<=n;j++){  //inner loop for columns
+    //         cout<< i;
+    //     }
+    //     cout << endl;
+    // }
+
+    /*012345
+    012345
+    012345
+    012345
+    012345*/
+    // int n = 5;
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 0;j<=n;j++){
+    //         cout<< j;
+    //     }
+    //     cout << endl;
+    // }
+
+    /******
+    *******
+    ********
+    ********
+    ******** */
+
+    // int n = 5;
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 0;j<=n;j++){
+    //         cout<< "*";
+    //     }
+    //     cout << endl;
+    // }
+
+    /* AAAAA
+    BBBBB
+    CCCCC
+    DDDDD
+    EEEEE */
+    // char ch = 'A';
+    // int n = 4;
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 0;j<=n;j++){
+    //         cout<< ch;
+    //     }
+    //     cout << endl;
+    //     ch++;
+    
+    // }
+
+
+    /*
+    ABCDE
+    FGHIJ
+    KLMNO
+    PQRST
+    */
+    // char ch = 'A';
+    // int n = 4;
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 0;j<=n;j++){
+    //         cout<< ch;
+    //         ch++;
+    //     }
+    //     cout << endl;
+    // }
+
+
+/*ABCDE
+ABCDE
+ABCDE
+ABCDE*/
+
+    // int n = 4;
+    // for(int i = 1;i<=n;i++){
+    //     char ch = 'A';
+    //     for(int j = 0;j<=n;j++){
+    //         cout<< ch;
+    //         ch++;
+    //     }
+    //     cout << endl;
+    
+    // }
+    return 0;
+}
---
+++ DSA Complate/LEC 4/patten_3.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+/*1 
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5*/
+    // int n = 5;
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 1;j<=i;j++){
+    //         cout<< i<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+/** 
+* *
+* * *
+* * * *
+* * * * * */
+    // int n = 5;
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 1;j<=i;j++){
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+
+/*
+A 
+B C
+D E F
+G H I J
+K L M N O*/
+
+    // int n = 5;
+    // char ch = 'A';
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 1;j<=i;j++){
+    //         cout<<ch<<" ";
+    //         ch++;
+    //     }
+    //     cout<<"\n";
+    // }
+
+
+/*
+A 
+A B
+A B C
+A B C D
+A B C D E*/
+    //   int n = 5;
+    // for(int i = 1;i<=n;i++){
+    //     char ch = 'A';
+    //     for(int j = 1;j<=i;j++){
+    //         cout<<ch<<" ";
+    //         ch++;
+    //     }
+    //     cout<<"\n";
+    // }
+
+/*
+A 
+B B
+C C C
+D D D D
+E E E E E*/
+    //   int n = 5;
+    // char ch = 'A';
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 1;j<=i;j++){
+    //         cout<<ch<<" ";
+    //     }
+    //     cout<<"\n";
+    //     ch++; // Increment character after each row
+    // }
+
+
+/*
+1 
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5*/
+
+    // int n = 5;
+    // for(int i = 1;i<=n;i++){
+    //     for(int j = 1; j < i+1 ;j++){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<"\n";
+
+    // }
+
+
+/*
+1 
+2 1
+3 2 1
+4 3 2 1
+5 4 3 2 1
+6 5 4 3 2 1*/
+    //   int n = 5;
+    // for(int i = 0;i<=n;i++){
+    //     for(int j = i+1; j > 0 ;j--){
+    //         cout<<j<<" ";
+    //     }
+    //     cout<<"\n";
+
+    // }
+    
+
+/*
+1 
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+16 17 18 19 20 21*/
+    //   int n = 5;
+    //   int num = 1;
+    // for(int i = 0;i<=n;i++){
+    //     for(int j = i+1; j > 0 ;j--){
+    //         cout<<num<<" ";
+    //         num++;
+    //     }
+    //     cout<<"\n";
+
+    // }    
+
+      int n = 5;
+      char ch = 'A';
+    for(int i = 0;i<=n;i++){
+        for(int j = i; j > 0 ;j--){
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<"\n";
+
+    }
+
+    return 0; 
+}

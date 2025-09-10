710e040d51b8c0be85ad52aa1aa60240aeb97a8e
Arjun-murmu <murmuarjun88@gmail.com>
2025-09-10 19:08:00 +0530

    Clean up Excel lock file and update .gitignore

---
+++ STREAMLIT_PYTHON/arjun-blog/page_two.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title("This is Page Two")
+st.write("Welcome to Page Two of the multi-page Streamlit app!")
+st.write("You can navigate back to the main page using the sidebar.")

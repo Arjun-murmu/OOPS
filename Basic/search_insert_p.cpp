48118d6c3af179fab0cc49529cb025ffa812e014
Arjun-murmu <murmuarjun88@gmail.com>
2025-11-24 02:00:23 +0530

    DSA Insert

--- Basic/search_insert_p.cpp
+++ Basic/search_insert_p.cpp
@@ -1,3 +1,7 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
@@ -19,3 +23,25 @@ public:
         return start;
     }
 };
+
+int main(){
+    // vector<int> nums = {2, 4, 5, 6, 7};
+    // int target = 4;
+    int n;
+    cin>>n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin>>nums[i];
+        nums.push_back(nums[i]);
+    }
+    int target;
+    cout<<"Enter target : ";
+    cin>>target;
+
+    Solution s;
+    int result = s.searchInsert(nums, target);
+    cout<<result;
+
+    return 0;
+
+}
